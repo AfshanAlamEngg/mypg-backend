e89cfb0b6841a31e3437fb6b621e087d4abcdc24
Afshan01 <afshanalamengg@gmail.com>
2024-06-06 15:22:56 +0530

    Modified Owner Management Module

--- backend/my_pg/owners/admin.py
+++ backend/my_pg/owners/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+# from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Owner
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contact_information', 'properties_owned', 'created_at', 'updated_at')
--- backend/my_pg/owners/models.py
+++ backend/my_pg/owners/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+# from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Owner(models.Model):
+    name = models.CharField(max_length=100)
+    contact_information = models.TextField()
+    properties_owned = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
